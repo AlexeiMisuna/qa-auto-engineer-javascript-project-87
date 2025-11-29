faea333735fb9acf77bbafad5cb6808a1c76c67d
AlekseyMisuna <alexei.misuna@yandex.ru>
2025-11-29 15:04:28 +0500

    fix for reliability

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,14 @@
 const buildDiff = (obj1, obj2) => {
-  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort()
+  // объединяем ключи обоих объектов, убираем дубликаты и сортируем по алфавиту
+  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort((a, b) => a.localeCompare(b))
 
   const diffTree = keys.map((key) => {
-    if (!(key in obj1)) return { key, type: 'added', value: obj2[key] }
-    if (!(key in obj2)) return { key, type: 'removed', value: obj1[key] }
+    if (!(key in obj1)) {
+      return { key, type: 'added', value: obj2[key] }
+    }
+    if (!(key in obj2)) {
+      return { key, type: 'removed', value: obj1[key] }
+    }
     if (obj1[key] !== obj2[key]) {
       return { key, type: 'changed', oldValue: obj1[key], newValue: obj2[key] }
     }
